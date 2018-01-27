dd33f16d6b5c5fcc6de12d892fc955bb4b41e3fa
xianchuanwu <1053831109>
2018-01-27 16:37:41 +0800

    init

--- src/main/java/org/solar/wechat/WeChatPay.java
+++ src/main/java/org/solar/wechat/WeChatPay.java
@@ -16,10 +16,10 @@ public class WeChatPay {
     public static final String tradeType_APP="APP";
     private String appid;
     private String mch_id;
-    private String notify_url;
     private String key;
+    private String notify_url;
 
-    public WeChatPay(String appid, String mch_id, String notify_url, String key) {
+    public WeChatPay(String appid, String mch_id, String key, String notify_url) {
         this.appid = appid;
         this.mch_id = mch_id;
         this.notify_url = notify_url;
@@ -79,6 +79,27 @@ public class WeChatPay {
      <sign>FDD167FAA73459FD921B144BAF4F4CA2</sign>
      </xml>
      * @return
+    <xml>
+    <return_code><![CDATA[SUCCESS]]></return_code>
+    <return_msg><![CDATA[OK]]></return_msg>
+    <appid><![CDATA[wx2421b1c4370ec43b]]></appid>
+    <mch_id><![CDATA[10000100]]></mch_id>
+    <device_info><![CDATA[1000]]></device_info>
+    <nonce_str><![CDATA[TN55wO9Pba5yENl8]]></nonce_str>
+    <sign><![CDATA[BDF0099C15FF7BC6B1585FBB110AB635]]></sign>
+    <result_code><![CDATA[SUCCESS]]></result_code>
+    <openid><![CDATA[oUpF8uN95-Ptaags6E_roPHg7AG0]]></openid>
+    <is_subscribe><![CDATA[Y]]></is_subscribe>
+    <trade_type><![CDATA[MICROPAY]]></trade_type>
+    <bank_type><![CDATA[CCB_DEBIT]]></bank_type>
+    <total_fee>1</total_fee>
+    <fee_type><![CDATA[CNY]]></fee_type>
+    <transaction_id><![CDATA[1008450740201411110005820873]]></transaction_id>
+    <out_trade_no><![CDATA[1415757673]]></out_trade_no>
+    <attach><![CDATA[订单额外描述]]></attach>
+    <time_end><![CDATA[20141111170043]]></time_end>
+    <trade_state><![CDATA[SUCCESS]]></trade_state>
+    </xml>
      */
     public String orderQuery(String out_trade_no){
         Map reqData=new HashMap();
